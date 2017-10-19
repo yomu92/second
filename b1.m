68d26ed8316f997c86dae2bd4aa6fda435bfdd3c
Johannes Jung <jung.johannes.92@gmail.com>
2017-10-19 12:55:42 +0200

    Add files via upload

---
+++ b1.m
@@ -0,0 +1,54 @@
+clear all
+method=3;
+sigma=0.8;
+km=10;
+
+var={10,50,100,150,250,300,500};
+
+
+for v=1:7
+    for k=2:10
+        tic
+        ARI=zeros(30,1);
+        for n=1:30
+           
+                error=1;
+                count=1;
+                while error==1
+                    try
+                        readM=sprintf('Variance %d/k%d/synth%dMv%dn%d.xlsx',var{v},k,k,var{v},n);
+                        M=xlsread(readM); 
+
+                        [Z,Z1]=kmeansf(M,k,km,sigma,method);
+                        tm=20;
+
+                        a=zeros(k*10,1);
+
+                         for i=1:k
+                                a(1+10*(i-1):10*i,1)=i;
+                         end
+
+                           t=a;
+
+
+                        ARI(n,1)=RandIndex(Z1,t);
+                        error=0;
+                    catch
+                        if count<100
+                            count=count+1;
+                        else
+                            disp('Error')
+                            return
+                        end
+                    end
+                end
+                
+                
+            
+        end
+        ARI
+        indexName=sprintf('SAVE/Variance %d/k%d/ARIk%dWv%dMaxiSigm%dMod.xlsx',var{v},k,k,var{v},sigma);
+        xlswrite(indexName, ARI);
+        toc
+    end
+end
